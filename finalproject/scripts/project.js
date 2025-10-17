654ab61f2e104aacd33708a3898506c9a5b389a4
jkagubare20 <kagubarejesus@gmail.com>
2025-10-17 20:05:35 +0200

    removed un used code

--- finalproject/scripts/project.js
+++ finalproject/scripts/project.js
@@ -27,20 +27,6 @@ closeButtons.forEach(button => {
     });
 });
 
-// Close dialog when clicking outside
-document.querySelectorAll('dialog').forEach(dialog => {
-    dialog.addEventListener('click', (e) => {
-        // const dialogDimensions = dialog.getBoundingClientRect();
-        // if (
-        //     e.clientX < dialogDimensions.left ||
-        //     e.clientX > dialogDimensions.right ||
-        //     e.clientY < dialogDimensions.top ||
-        //     e.clientY > dialogDimensions.bottom
-        // ) {
-            dialog.close();
-        });
-    });
-
 // Form Handling with LocalStorage
 const form = document.getElementById('application-form');
 
